be4543664abe585da8fa3d846dc718e5d3cd98f2
p8324r <112684234+p8324r@users.noreply.github.com>
2023-07-05 02:25:05 +0530

    Update firebaseconfig.js

--- server/firebaseconfig.js
+++ server/firebaseconfig.js
@@ -1,4 +1,5 @@
 const admin = require('firebase-admin');
+// Modify the path in the `require()` to your private key file 
 const serviceAccount = require('./Privatekey/messenger-332fe-firebase-adminsdk-j2238-08d9210514.json');
 
 admin.initializeApp({
